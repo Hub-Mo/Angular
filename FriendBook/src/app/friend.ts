6d183661aecbd2d6bb13476719fadee798688682
Hub-Mo <Mohamed.elja@hotmail.com>
2022-08-04 16:59:41 +0200

    added extra error handlers for regex recognition

--- FriendBook/src/app/friend.ts
+++ FriendBook/src/app/friend.ts
@@ -1,43 +1,19 @@
 export class Friend {
-  private _firstName : string;
-  private _lastName : string;
-  private _phone : number;
-  private _email : string;
-  private _favLang : string;
-  private _message : string;
+   firstName : string | null;
+   lastName : string | null;
+   phone : number | null;
+   email : string | null;
+   favLang : string | null;
+   message : string | null;
 
 
 
-  constructor(firstName : string, lastName : string, phone : number, email : string, favLang : string, message : string) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._phone = phone;
-    this._email = email;
-    this._favLang = favLang;
-    this._message = message;
-  }
-
-  get friendFirstName(): string {
-    return this._firstName;
-  }
-
-  get lastName(): string {
-    return this._lastName;
-  }
-
-  get phone(): number {
-    return this._phone;
-  }
-
-  get friendEmail(): string {
-    return this._email;
-  }
-
-  get favLang(): string {
-    return this._favLang;
-  }
-
-  get message(): string {
-    return this._message;
+  constructor(firstName : string  | null, lastName : string | null, phone : number | null, email : string | null, favLang : string | null, message : string | null) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.email = email;
+    this.favLang = favLang;
+    this.message = message;
   }
 }
